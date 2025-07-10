8f8962cbd70aaf6151093c80872401cbcb384eb6
abhay113 <abhay@cateina.com>
2025-07-10 12:30:43 +0530

    feat: changed eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,9 +3,24 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.browser } },
+  // Configuration for JavaScript/TypeScript files
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    languageOptions: { globals: globals.browser },
+  },
+  // TypeScript ESLint configuration
   tseslint.configs.recommended,
+  {
+    // Adding custom rules
+    rules: {
+      // Disabling the 'no-explicit-any' rule globally
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
 ]);
